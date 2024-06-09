b9ef4397f8a3a182043b831b5e36f66e55b73b53
chris-1187 <christian.munz@posteo.de>
2024-06-09 20:21:52 +0200

    final refactoring

--- main.py
+++ main.py
@@ -146,7 +146,6 @@ if __name__ == "__main__":
         create_tpch_data(sf)
         print("> data successfully created\n")
         print(f"\nQueries created: {queries_created}\n")
-        print(substrait_queries)
 
         #query_set = input("Enter query set (tpch_sql_original | tpch_sql_reduced): ")
 
@@ -438,8 +437,6 @@ if __name__ == "__main__":
                             pass
 
         queries_created = True
-
-
 
     print("\n\n\nBenchmark is completed !\n\nThe exported Benchmark results can be found at your local repository in the folder benchmark_results\n")
 
